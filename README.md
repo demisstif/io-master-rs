e257577436a3d041fb5d1b15ee40ee616e8c6e09
demisstif <demisstif@gmail.com>
2020-08-08 12:25:29 +0800

    write to file

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # io-master-rs
 
 A project to practice io in rust.
+
+## Basic
+
+### 1. write a sentence from terminal to a file
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::File;
+use std::io::{self, Write};
 fn main() {
-    println!("Hello, world!");
+    write_to_file();
+}
+
+fn write_to_file() {
+    // 1. get sentence from ternimal
+    io::stdout()
+        .write_all(b"Please input your sentence:\n")
+        .expect("Write tips error");
+    let mut sentence = String::new();
+    io::stdin()
+        .read_line(&mut sentence)
+        .expect("Some Error when read sentence from terminal");
+    println!("Got sentence from terminal: {}", sentence);
+    // 2. write to file
+    // 2.1 supply a file path
+    io::stdout()
+        .write("Please supply a file path:".as_bytes())
+        .expect("Write tips of file path failed");
+    // write is just place data to buf and wait to write, so there nedd to flush it
+    io::stdout().flush().expect("Flush data");
+    let mut file_path = String::new();
+    io::stdin()
+        .read_line(&mut file_path)
+        .expect("Get file path failed");
+    let file_path = file_path.trim();
+    let mut f = File::create(file_path).expect("Create file failed");
+    f.write_all(sentence.as_bytes())
+        .expect("Write to file failed");
 }
